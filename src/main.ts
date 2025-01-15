55d4c1b471c6e0722c8cee51bea6eac31dc716ec
jordan-Florez <jordansaldarriaga14@gmail.com>
2025-01-15 13:47:22 -0500

    Solving the case sensitivity of the API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
+// bootstrapApplication(AppComponent, appConfig)
+//   .catch((err) => console.error(err));
+
+// Aquí agregamos HttpClientModule directamente en appConfig
+bootstrapApplication(AppComponent, {
+  ...appConfig,  // Preservamos las configuraciones existentes
+  providers: [
+    ...appConfig.providers,  // Si ya tienes proveedores configurados en appConfig
+    importProvidersFrom(HttpClientModule)  // Importamos HttpClientModule
+  ]
+})
   .catch((err) => console.error(err));
